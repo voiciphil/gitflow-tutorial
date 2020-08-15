f9e3b79d915b3bbc648be4462d662ec8a39021b5
voiciphil <voiciphil@gmail.com>
2020-08-16 00:23:00 +0900

    test: Update test code

--- hello.c
+++ hello.c
@@ -6,9 +6,10 @@ int main(void) {
 
     struct vector v;
     init(&v);
+    printf("%d\n", empty(&v));
     for (int i = 0; i < 10; i++) push_back(&v, i);
     for (int i = 0; i < 10; i++) printf("%d ", *at(&v, i));
-    printf("\n");
-    
+    printf("\n%d\n", empty(&v));
+
     return 0;
 }
