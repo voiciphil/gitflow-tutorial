9562bf60844e82ef2e0fbef9f16168b4aa44cec2
voiciphil <voiciphil@gmail.com>
2020-08-15 23:58:59 +0900

    feat: Add push_back function

--- vector.c
+++ vector.c
@@ -6,3 +6,12 @@ void init(struct vector *v) {
     v->cur = 0;
     v->cap = 1;
 }
+
+void push_back(struct vector *v, int val) {
+    if (v->cur == v->cap) {
+        v->arr = realloc(v->arr, sizeof(int) * v->cap * 2);
+    }
+
+    v->cap *= 2;
+    v->arr[v->cur++] = val;
+}
