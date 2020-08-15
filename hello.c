e7f022319989a83cad733aa14d13bf5f35b1fc0f
voiciphil <voiciphil@gmail.com>
2020-08-16 00:20:49 +0900

    test: Add test code for vector

--- hello.c
+++ hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "vector.h"
 
 int main(void) {
     printf("hello, world!\n");
+
+    struct vector v;
+    init(&v);
+    for (int i = 0; i < 10; i++) push_back(&v, i);
+    for (int i = 0; i < 10; i++) printf("%d ", *at(&v, i));
+    printf("\n");
+    
     return 0;
 }
